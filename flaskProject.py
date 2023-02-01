f00832b574174390872da70eb3cbc61f06df5971
Mohammed El Yousfi <103375272+mohammedelyousfi@users.noreply.github.com>
2023-02-01 16:39:33 +0100

    method to read the csv file.
    adding the csv file reader method to extract information about the transactions and people who are part of each transaction

--- flaskProject.py
+++ flaskProject.py
@@ -37,3 +37,22 @@ class Transaction:
 #creation des deux tables persons et transactions qui seront remplies des infos recupérées du fichier csv fourni
 persons = []
 transactions = []
+
+#recupérer les infos du fichiers csv
+def load_data_from_csv(file_path):
+    with open(file_path, "r") as f:
+        reader = csv.reader(f, delimiter=";")
+        for row in reader:
+            sender, recipient, amount = row
+            sender_person = next((p for p in persons if p.name == sender), None)
+            if not sender_person:
+                sender_person = Person(sender, 0)
+                persons.append(sender_person)
+            recipient_person = next((p for p in persons if p.name == recipient), None)
+            if not recipient_person:
+                recipient_person = Person(recipient, 0)
+                persons.append(recipient_person)
+            transaction = Transaction(sender, recipient, int(amount))
+            transactions.append(transaction)
+            sender_person.debit(int(amount))
+            recipient_person.credit(int(amount))
