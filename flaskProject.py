8455c1f24b40296d5b58191ad9c59a070ab84723
Mohammed El Yousfi <103375272+mohammedelyousfi@users.noreply.github.com>
2023-02-01 16:50:00 +0100

    main method to run the flask App

--- flaskProject.py
+++ flaskProject.py
@@ -91,3 +91,15 @@ def printAll():
         return res+"</ul>"
     else:
         return "Invalid request method"
+    
+if __name__ == "__main__":
+    print(transactions)
+    if len(sys.argv) > 1 :
+        if sys.argv[1] == "check_syntax":
+            print("Build [ OK ]")
+            exit(0)
+        else:
+            print("Passed argument not supported ! Supported argument : check_syntax")
+            exit(1)
+    app.run(debug=True)
+
